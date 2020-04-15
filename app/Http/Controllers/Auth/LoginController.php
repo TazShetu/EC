1ed5bd34fda52c125d62670c3f00b06ce75d7530
Unknown <tanmoontaz@gmail.com>
2020-04-15 17:08:27 +0600

    facebook login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\Controller;
 //use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -17,6 +21,10 @@ class LoginController extends Controller
 //    protected $redirectTo = RouteServiceProvider::HOME;
     protected $redirectTo = '/home';
 
+    protected $providers = [
+//        'github','facebook','google','twitter'
+        'facebook','google',
+    ];
 
     public function __construct()
     {
@@ -38,6 +46,98 @@ class LoginController extends Controller
         } else {
             return redirect()->route('login')->with('error', 'Username Or Email-Address And Password do not Match.');
         }
+    }
+
+
+    public function show()
+    {
+        return view('auth.login');
+    }
+
+    public function redirectToProvider($driver)
+    {
+        if( ! $this->isProviderAllowed($driver) ) {
+            return $this->sendFailedResponse("{$driver} is not currently supported");
+        }
+
+        try {
+            return Socialite::driver($driver)->redirect();
+        } catch (Exception $e) {
+            // You should show something simple fail message
+            return $this->sendFailedResponse($e->getMessage());
+        }
+    }
+
+
+    public function handleProviderCallback( $driver )
+    {
+        try {
+            $user = Socialite::driver($driver)->user();
+        } catch (Exception $e) {
+            return $this->sendFailedResponse($e->getMessage());
+        }
+
+        // check for email in returned user
+        return empty( $user->email )
+            ? $this->sendFailedResponse("No email id returned from {$driver} provider.")
+            : $this->loginOrCreateAccount($user, $driver);
+    }
+
+    protected function sendSuccessResponse()
+    {
+        return redirect()->intended('home');
+    }
+
+    protected function sendFailedResponse($msg = null)
+    {
+        return redirect()->route('social.login')
+            ->withErrors(['msg' => $msg ?: 'Unable to login, try with another provider to login.']);
+    }
+
+    protected function loginOrCreateAccount($providerUser, $driver)
+    {
+        // check for already has account
+        $user = User::where('email', $providerUser->getEmail())->first();
+
+        // if user already found
+        if( $user ) {
+            // update the avatar and provider that might have changed
+            $user->update([
+                'avatar' => $providerUser->avatar,
+                'provider' => $driver,
+                'provider_id' => $providerUser->id,
+                'access_token' => $providerUser->token
+            ]);
+        } else {
+            //Check email exists or not. If exists create a new user
+            if($providerUser->getEmail()){
+                // create a new user
+                $user = User::create([
+                    'name' => $providerUser->getName(),
+                    'email' => $providerUser->getEmail(),
+                    'username' => $providerUser->getEmail(),
+                    'avatar' => $providerUser->getAvatar(),
+                    'provider' => $driver,
+                    'provider_id' => $providerUser->getId(),
+                    'access_token' => $providerUser->token,
+                    // user can use reset password to create a password
+                    'password' => ''
+                ]);
+            }else{
+                //Show message here what you want to show
+                return redirect()->route('login')->with('error', 'No Email address was found in facebook :(');
+            }
+        }
+
+        // login the user
+        Auth::login($user, true);
+
+        return $this->sendSuccessResponse();
+    }
+
+    private function isProviderAllowed($driver)
+    {
+        return in_array($driver, $this->providers) && config()->has("services.{$driver}");
     }
 
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
@@ -20,16 +16,18 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('avatar')->nullable();
+            $table->string('provider', 20)->nullable();
+            $table->string('provider_id')->nullable();
+            $table->string('access_token')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('users');
